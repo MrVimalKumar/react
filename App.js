82ef65e3b3f99d789cc53a27b5681018b698d810
MrVimalKumar <vimal.cst.2021@snsce.ac.in>
2024-11-26 08:27:12 +0530

    Created React Component

--- App.js
+++ App.js
@@ -15,5 +15,14 @@ import ReactDOM from "react-dom/client";
 
 const heading = (<h1 id="heading">I am Vimal Kumar R</h1>);
 
+const Heading1 = () => <h1>I have Completed 3 Epdisode of Javascript in Namaste React Course</h1>
+
+const Heading2 = () =>(
+  <div>
+    {heading}
+    <h1 className="heading">I am Studying React JS from Namaste React course</h1>
+    <Heading1/>
+  </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading2/>);
